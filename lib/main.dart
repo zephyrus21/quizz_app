1c174c31746c1bf5dda15f47eaea5fc6351e84f5
zephyrus21 <pandey.piyus@gmail.com>
2021-08-03 00:58:26 +0530

    adding widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,51 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var qIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      qIndex++;
+      print(qIndex);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite number?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quizz App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[qIndex]),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: answerQuestion, child: Text("Answer 1")),
+                ElevatedButton(
+                    onPressed: answerQuestion, child: Text("Answer 2")),
+                ElevatedButton(
+                    onPressed: answerQuestion, child: Text("Answer 3")),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
